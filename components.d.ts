96efae38f163aba0cc3056e5bab2261d2f17ffea
Plumbiu <3434909403@qq.com>
2023-06-09 20:16:48 +0800

    feat: add env files

--- components.d.ts
+++ components.d.ts
@@ -42,5 +42,7 @@ declare module 'vue' {
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTimePicker: typeof import('element-plus/es')['ElTimePicker']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
   }
 }
