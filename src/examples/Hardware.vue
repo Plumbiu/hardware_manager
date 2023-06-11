eeb9b16c486ec3d425375cbe62fab550a4a0a803
Plumbiu <3434909403@qq.com>
2023-06-11 23:41:30 +0800

    feat: 增添所有器件

--- src/examples/Hardware.vue
+++ src/examples/Hardware.vue
@@ -1,39 +1,46 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import request from '../utils/request'
-import type { ISpanMethod, IHardware } from './types'
-
-const {
-  data: { data: hardwareJSON },
-} = await request.get('/')
+import type { IHardware } from './types'
+// 分页数据
+const pagesize = 35
+const pagenum = ref(1)
+const { data: { data: hardwareJSON } } = await request.get('/')
+const isLoading = ref(false)
+const total = ref(hardwareJSON.length)
 // 表格数据
-const hardware = ref(hardwareJSON)
+const hardware = ref(hardwareJSON.slice(0, pagesize))
 // 查询表单数据
-const searchForm = ref({
-  name: '',
-  type: '',
-})
-// 合并盒子序号的配置数据
-const spanMethod = ({ rowIndex, columnIndex }: ISpanMethod) => {
-  if (columnIndex === 0) {
-    if (rowIndex % 18 === 0) {
-      return [18, 1]
+const searchForm = ref({ name: '', type: '' })
+watch([pagenum, searchForm], (newValue) => {
+  const filterHardware = hardwareJSON.filter((item: { name: string; type: string; }) => {
+    const originName = item.name.toLowerCase()
+    const findName = searchForm.value.name.toLowerCase()
+    const originType = item.type.toLowerCase()
+    const findType = searchForm.value.type.toLowerCase()
+    if (searchForm.value.name === '') {
+      return originType.includes(findType)
     }
-    return [0, 0]
-  }
-}
+    if (searchForm.value.type === '') {
+      return originName.includes(findName)
+    }
+    return originName.includes(findName) && originType.includes(findType)
+  })
+  hardware.value = filterHardware.slice((newValue[0] - 1) * pagesize, newValue[0] * pagesize)
+  total.value = filterHardware.length
+}, { deep: true })
 // 修改弹出框的显示与隐藏
 const visible = ref(false)
 // 更新框数据
 const updatedHardware = ref<IHardware>()
+function currentChangeHandler(newPagenum: number) {
+  pagenum.value = newPagenum
+}
 // 重新请求函数
 async function reFetch() {
-  const {
-    data: { data: hardwareJSON },
-  } = await request.get('/')
+  const { data: { data: hardwareJSON }} = await request.get('/')
   hardware.value = hardwareJSON
 }
-const isLoading = ref(false)
 // 控制硬件操作按钮的函数
 // type 为 update 表示要更新，type 为 delete 表示要删除
 async function handlerEdit(rowData: any, type: 'update' | 'delete') {
@@ -44,12 +51,9 @@ async function handlerEdit(rowData: any, type: 'update' | 'delete') {
     // 删除操作
     try {
       isLoading.value = true
-      const {
-        data: { message },
-      } = await request.delete(`/${rowData.id}`)
+      const { data: { message } } = await request.delete(`/${rowData.id}`)
       ElMessage.success(message)
       await reFetch()
-
     } catch (error) {
       console.log('err')
     } finally {
@@ -58,23 +62,6 @@ async function handlerEdit(rowData: any, type: 'update' | 'delete') {
   }
 }
 // 搜索框的搜索函数
-function searchHandler() {
-  hardware.value = hardwareJSON.filter(
-    (item: { name: string; type: string }) => {
-      const originName = item.name.toLowerCase()
-      const findName = searchForm.value.name.toLowerCase()
-      const originType = item.type.toLowerCase()
-      const findType = searchForm.value.type.toLowerCase()
-      if (searchForm.value.name === '') {
-        return originType.includes(findType)
-      }
-      if (searchForm.value.type === '') {
-        return originName.includes(findName)
-      }
-      return originName.includes(findName) && originType.includes(findType)
-    }
-  )
-}
 // 修改框的操作，type 为 submit 表示用户确认了修改，type 为 cancel 表示用户取消了修改
 async function handleUpdateClose(type: 'submit' | 'cancel') {
   if (type === 'submit') {
@@ -82,12 +69,7 @@ async function handleUpdateClose(type: 'submit' | 'cancel') {
     const { id, name, type, row, col, box_num } = updatedHardware.value!
     try {
       await request.put(`/${id}`, {
-        id,
-        name,
-        type,
-        row,
-        col,
-        box_num,
+        id, name, type, row, col, box_num
       })
       await reFetch()
     } catch (error) {
@@ -105,18 +87,10 @@ async function handleUpdateClose(type: 'submit' | 'cancel') {
     <div class="form_container">
       <el-form :model="searchForm">
         <el-form-item label="器件名称">
-          <el-input
-            placeholder="请输入器件名称"
-            v-model="searchForm.name"
-            @input="searchHandler"
-          />
+          <el-input placeholder="请输入器件名称" v-model="searchForm.name" />
         </el-form-item>
         <el-form-item label="器件类型">
-          <el-select
-            style="padding: 0"
-            @change="searchHandler"
-            v-model="searchForm.type"
-          >
+          <el-select style="padding: 0" v-model="searchForm.type">
             <el-option label="全部" value="" />
             <el-option label="IC" value="IC" />
             <el-option label="5A" value="5A" />
@@ -125,14 +99,71 @@ async function handleUpdateClose(type: 'submit' | 'cancel') {
             <el-option label="接口" value="接口" />
             <el-option label="PNP" value="PNP" />
             <el-option label="晶振" value="晶振" />
+            <el-option label="1117" value="1117" />
+            <el-option label="CAP" value="CAP" />
+            <el-option label="FET" value="FET" />
+            <el-option label="IND" value="IND" />
+            <el-option label="LED" value="LED" />
+            <el-option label="MOS管" value="MOS管" />
+            <el-option label="NPN" value="NPN" />
+            <el-option label="PNP" value="PNP" />
+            <el-option label="RES" value="RES" />
+            <el-option label="SNAP MAKER" value="SNAP MAKER" />
+            <el-option label="TF卡座" value="TF卡座" />
+            <el-option label="USB" value="USB" />
+            <el-option label="XTAL" value="XTAL" />
+            <el-option label="按钮" value="按钮" />
+            <el-option label="保险丝" value="保险丝" />
+            <el-option label="插头" value="插头" />
+            <el-option label="插座" value="插座" />
+            <el-option label="传感器" value="传感器" />
+            <el-option label="串口通信模块" value="串口通信模块" />
+            <el-option label="磁铁" value="磁铁" />
+            <el-option label="磁珠" value="磁珠" />
+            <el-option label="底座" value="底座" />
+            <el-option label="电感" value="电感" />
+            <el-option label="电机" value="电机" />
+            <el-option label="电容" value="电容" />
+            <el-option label="电位器" value="电位器" />
+            <el-option label="电阻" value="电阻" />
+            <el-option label="垫圈" value="垫圈" />
+            <el-option label="顶针" value="顶针" />
+            <el-option label="端子" value="端子" />
+            <el-option label="舵机" value="舵机" />
+            <el-option label="耳机插座" value="耳机插座" />
+            <el-option label="二极管" value="二极管" />
+            <el-option label="干电池" value="干电池" />
+            <el-option label="基本元器件" value="基本元器件" />
+            <el-option label="继电器" value="继电器" />
+            <el-option label="接口" value="接口" />
+            <el-option label="接头" value="接头" />
+            <el-option label="接线柱" value="接线柱" />
+            <el-option label="镜头" value="镜头" />
+            <el-option label="卡座" value="卡座" />
+            <el-option label="开关" value="开关" />
+            <el-option label="密封圈" value="密封圈" />
+            <el-option label="模块" value="模块" />
+            <el-option label="排母" value="排母" />
+            <el-option label="排针" value="排针" />
+            <el-option label="三极管" value="三极管" />
+            <el-option label="摄像头" value="摄像头" />
+            <el-option label="弯折铝管" value="弯折铝管" />
+            <el-option label="无线仿真器" value="无线仿真器" />
+            <el-option label="扬声器" value="扬声器" />
+            <el-option label="语音合成模块" value="语音合成模块" />
+            <el-option label="直插式电解电容" value="直插式电解电容" />
+            <el-option label="直插式铝电解电容" value="直插式铝电解电容" />
+            <el-option label="钻头" value="钻头" />
           </el-select>
         </el-form-item>
       </el-form>
       <el-pagination
+        @current-change="currentChangeHandler"
         small
         background
         layout="prev, pager, next"
-        :total="50"
+        :page-size="pagesize"
+        :total="total"
       />
     </div>
     <el-table
@@ -140,7 +171,6 @@ async function handleUpdateClose(type: 'submit' | 'cancel') {
       border
       fit
       :data="hardware"
-      :span-method="spanMethod"
       style="flex: 1; margin-bottom: 25px"
     >
       <el-table-column
--- src/examples/types.ts
+++ src/examples/types.ts
@@ -1,8 +1,20 @@
+import type { TableColumnCtx } from 'element-plus'
+interface IHardware {
+  id: number
+  name: string
+  type: string
+  box_num: string
+  col: string
+  row: string
+  createdAt: Date
+}
+
 interface ISpanMethod {
+  row: IHardware
+  column: TableColumnCtx<IHardware>
   rowIndex: number
   columnIndex: number
 }
-
 interface IHardware {
   id:        number
   createdAt: Date
